730deb69a1a9e2b35a7207a2f52e01ff3fd5cbdc
Feilipu <Turgon.Telrunya@protonmail.com>
2021-02-04 15:47:10 +0100

    Ajout du tableau  et réécriture du tableau en format json pour permettre l'affichage des lives

--- view/books.php
+++ view/books.php
@@ -6,8 +6,13 @@
 	</head>
 	<body>
 		<h1>Bibliothèque à vapeur</h1>
+		<?php
+		$isbn = ["2354083181", "235408322X", "2354083254", "2371021369", "2371021350"];
+		?>
 		<script type="text/javascript" src="https://www.google.com/books/jsapi.js"></script>
-		<script type="text/javascript"></script>
+		<script>
+			let isbn = <?= json_encode($isbn); ?>;
+		</script>
 		<script src="assets/js/getBooks.js"></script>
 	</body>
 </html>
