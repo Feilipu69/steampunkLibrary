f268ca77bf530a9cc9c661d29f061c2128ab7152
Feilipu <Turgon.Telrunya@protonmail.com>
2021-02-09 16:48:01 +0100

    Création du fichier getABook.js

---
+++ assets/js/getABook.js
@@ -0,0 +1,28 @@
+let div = document.createElement("div");
+div.setAttribute("id", isbn);
+let titleElement = document.createElement("p");
+let img = document.createElement("img");
+let authorElement = document.createElement("p");
+let publisherElement = document.createElement("p");
+let publishedDateElement = document.createElement("p");
+let descriptionElement = document.createElement("p");
+fetch("https://www.googleapis.com/books/v1/volumes?q=" + isbn)
+	.then((response) => response.json())
+	.then((data) => {
+		titleElement.textContent = data.items[0].volumeInfo.title;
+		authorElement.textContent = data.items[0].volumeInfo.authors;
+		publisherElement.textcontent = data.items[0].volumeInfo.publisher;
+		publishedDateElement.textContent = data.items[0].volumeInfo.publishedDate;
+		img.src = data.items[0].volumeInfo.imageLinks.thumbnail;
+		img.alt = "couverture " + data.items[0].volumeInfo.title;
+		descriptionElement.textContent = data.items[0].volumeInfo.description;
+		document.body.appendChild(div);
+		div.appendChild(titleElement);
+		div.appendChild(authorElement);
+		div.appendChild(publisherElement);
+		div.appendChild(publishedDateElement);
+		div.appendChild(img);
+		div.appendChild(descriptionElement);
+	})
+	.catch((error) => console.log("Erreur " + error));
+
