10026b0ca073d8ed0e1e33926fae5372d6c5ab62
Feilipu <Turgon.Telrunya@protonmail.com>
2021-02-10 11:26:39 +0100

    Création de la classe Opinion

---
+++ src/model/Opinion.php
@@ -0,0 +1,57 @@
+<?php
+namespace Bihin\steampunkLibrary\src\model;
+
+class Opinion
+{
+	private $id;
+	private $isbn;
+	private $comment;
+	private $dateOfComment;
+
+	public function __construct(array $data){
+		$this->hydrate($data);
+	}
+
+	public function hydrate(array $data){
+		foreach($data as $key => $value){
+			$method = 'set'.ucfirst($key);
+			if (method_exists($this, $method)) {
+				$this->$method($value);
+			}
+		}
+	}
+
+	public function getId(){
+		return $this->id;
+	}
+
+	public function getIsbn(){
+		return $this->isbn;
+	}
+
+	public function getComment(){
+		return $this->comment;
+	}
+
+	public function getDateOfComment(){
+		return $this->dateOfComment;
+	}
+
+	public function setId(int $id){
+		if ($id > 0) {
+			$this->id = $id;
+		}
+	}
+
+	public function setIsbn($isbn){
+		$this->isbn = $isbn;
+	}
+
+	public function comment($comment){
+		$this->comment = $comment;
+	}
+
+	public function dateOfComment($dateOfComment){
+		$this->dateOfComment = $dateOfComment;
+	}
+}
