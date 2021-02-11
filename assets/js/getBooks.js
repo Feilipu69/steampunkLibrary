b18b0cdc579c8a640d6339a0e6e82dc5ac919d64
Feilipu <Turgon.Telrunya@protonmail.com>
2021-02-11 09:40:55 +0100

    Modification du href ligne 12 avec l'ajout de book

--- assets/js/getBooks.js
+++ assets/js/getBooks.js
@@ -1,25 +1,24 @@
-async function livres(){
-	for (isbn of catalogue) {
-		let div = document.createElement("div");
-		div.setAttribute("id", isbn);
-		let titleElement = document.createElement("p");
-		let linkElement = document.createElement("a");
-		let img = document.createElement("img");
-		await fetch("https://www.googleapis.com/books/v1/volumes?q=" + isbn)
-			.then((response) => response.json())
-			.then((data) => {
-				titleElement.textContent = data.items[0].volumeInfo.title;
-				linkElement.href = host + "/" + isbn;
-				img.src = data.items[0].volumeInfo.imageLinks.thumbnail;
-				img.alt = "couverture " + data.items[0].volumeInfo.title;
-				document.body.appendChild(div);
-				div.appendChild(titleElement);
-				div.appendChild(linkElement);
-				linkElement.appendChild(img);
-			})
-			.catch((error) => console.log("Erreur " + error));
-	}
+async function livres() {
+  for (isbn of catalogue) {
+    let div = document.createElement("div");
+    div.setAttribute("id", isbn);
+    let titleElement = document.createElement("p");
+    let linkElement = document.createElement("a");
+    let img = document.createElement("img");
+    await fetch("https://www.googleapis.com/books/v1/volumes?q=" + isbn)
+      .then((response) => response.json())
+      .then((data) => {
+        titleElement.textContent = data.items[0].volumeInfo.title;
+        linkElement.href = `${host}/book/${isbn}`;
+        img.src = data.items[0].volumeInfo.imageLinks.thumbnail;
+        img.alt = "couverture " + data.items[0].volumeInfo.title;
+        document.body.appendChild(div);
+        div.appendChild(titleElement);
+        div.appendChild(linkElement);
+        linkElement.appendChild(img);
+      })
+      .catch((error) => console.log("Erreur " + error));
+  }
 }
 
 livres();
-
