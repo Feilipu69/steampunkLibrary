4174520ae383ff64a527e737cb44914e2d1b7bf0
Feilipu <Turgon.Telrunya@protonmail.com>
2021-02-15 16:07:19 +0100

    Création du formulaire de mise à jour des données du membre + bouton de suppression du compte

---
+++ view/updateData.php
@@ -0,0 +1,24 @@
+<h3>Modification de vos données</h3>
+<form method="post" action="<?= HOST; ?>/account">
+	<label for="login">Pseudo : </label>
+	<input type="text" name="login" required />
+	<br>
+	<label for="password">Mot de passe : </label>
+	<input type="password" name="password" required />
+	<br>
+	<label for="mail">Courriel : </label>
+	<input type="email" name="email" required />
+	<br>
+	<input type="submit" name="account" value="Modifications" />
+	<button onclick="window.location.href='<?= HOST; ?>/account';">Annuler</button>
+	<button onclick="window.location.href='<?= HOST; ?>/deleteSubscriber';">Suppression</button>
+</form>
+
+<?php
+if (isset($_SESSION['registerError'])) {
+	?>
+	<div><?= $_SESSION['registerError']; ?></div>
+	<?php
+}
+?>
+
