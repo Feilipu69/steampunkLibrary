30f4ea741fcebafd9ce13c44f74c02519e2f7317
Feilipu <Turgon.Telrunya@protonmail.com>
2021-02-16 16:41:40 +0100

    Création de la classe BooksCatalogueManager et des méthodes getBooks et addABook

---
+++ src/DAO/BooksCatalogueManager.php
@@ -0,0 +1,23 @@
+<?php
+namespace Bihin\steampunkLibrary\src\DAO;
+
+use Bihin\steampunkLibrary\src\DAO\DbConnect;
+use Bihin\steampunkLibrary\src\model\BooksCatalogue;
+
+class BooksCatalogueManager extends DbConnect
+{
+	public function getBooks(){
+		$req = $this->db->query('SELECT * FROM booksCatalogue');
+		while ($data = $req->fetch()) {
+			$books[] = new BooksCatalogue($data);
+		}
+		return $books;
+	}
+	public function addABook($post){
+		$book = new BooksCatalogue($post);
+		$req = $this->db->prepare('INSERT INTO booksCatalogue (isbn) VALUES (:isbn)');
+		$req->execute([
+			'isbn' => $book->getIsbn()
+		]);
+	}
+}
