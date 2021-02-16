b2b3d06a2cc16c245d1e6f18b1c4a5ff564bd103
Feilipu <Turgon.Telrunya@protonmail.com>
2021-02-16 16:44:23 +0100

    Création du formulaire d'ajout d'un livre

---
+++ view/newBook.php
@@ -0,0 +1,6 @@
+<h3>Nouveau livre</h3>
+<form method="post" action="<?= HOST; ?>/addABook">
+	<label for="isbn">Isbn</label>
+	<input type="text" name="isbn" />
+	<input type="submit" name="addABook" value="Envoyer" />
+</form>
