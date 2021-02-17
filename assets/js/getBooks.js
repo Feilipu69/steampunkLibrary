2371a408a9666094b3013893bd59119e5a0dc837
Feilipu <Turgon.Telrunya@protonmail.com>
2021-02-17 14:47:38 +0100

    Lignes 4 et 8 modification de isbn par parameter

--- assets/js/getBooks.js
+++ assets/js/getBooks.js
@@ -1,15 +1,15 @@
 async function livres() {
-  for (isbn of catalogue) {
+  for (parameter of catalogue) {
     let div = document.createElement("div");
-    div.setAttribute("id", isbn);
+    div.setAttribute("id", parameter);
     let titleElement = document.createElement("p");
     let linkElement = document.createElement("a");
     let img = document.createElement("img");
-    await fetch("https://www.googleapis.com/books/v1/volumes?q=" + isbn)
+    await fetch("https://www.googleapis.com/books/v1/volumes?q=" + parameter)
       .then((response) => response.json())
       .then((data) => {
         titleElement.textContent = data.items[0].volumeInfo.title;
-        linkElement.href = `${host}/book/${isbn}`;
+        linkElement.href = `${host}/book/${parameter}`;
         img.src = data.items[0].volumeInfo.imageLinks.thumbnail;
         img.alt = "couverture " + data.items[0].volumeInfo.title;
         document.body.appendChild(div);
