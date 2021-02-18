dfd26e95f424f50b2f224b59bc5387527a1a3c94
Feilipu <Turgon.Telrunya@protonmail.com>
2021-02-18 08:19:22 +0100

    Création des méthodes administration, moderator et member

--- src/controller/AdminController.php
+++ src/controller/AdminController.php
@@ -1,11 +1,26 @@
 <?php
 namespace Bihin\steampunkLibrary\src\controller;
 
-use Bihin\steampunkLibrary\src\DAO\BooksCatalogueManager;	
+use Bihin\steampunkLibrary\src\DAO\{
+	BooksCatalogueManager,
+	SubscriberManager
+};
+
 use Bihin\steampunkLibrary\utils\View;
 
 class AdminController
 {
+	public function administration(){
+		// récupérer tout ce doit être afficher
+		$subscribers = new SubscriberManager();
+		$allSubscribers = $subscribers->getSubscribers();
+
+		$displayData = new View('administration');
+		$displayData->render([
+			'allSubscribers' => $allSubscribers
+		]);
+	}
+
 	public function addABook($book){
 		if (isset($book['addABook'])) {
 			if (!empty($book['isbn'])) {
@@ -16,5 +31,17 @@ class AdminController
 
 		$displayForm = new View('newBook');
 		$displayForm->render([]);
+	}
+
+	public function moderator($id){
+		$moderator = new SubscriberManager();
+		$newModerator = $moderator->moderator($id);
+		header('Location:' . HOST . '/administration');
+	}
+
+	public function member($id){
+		$membre = new SubscriberManager();
+		$newMembre = $membre->member($id);
+		header('Location:' . HOST . '/administration');
 	}
 }
