58e5a93e0f24cff9c5235f558da42889bcf420ba
Feilipu <Turgon.Telrunya@protonmail.com>
2021-02-21 17:03:35 +0100

    Ajout lignes 6 et 7 pour utilisation de TinyMCE

--- view/template.php
+++ view/template.php
@@ -3,6 +3,8 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+		<script>tinymce.init({selector:'textarea'});</script>
 		<title>La Bibliothèque à Vapeur</title>
 	</head>
 	<body>
