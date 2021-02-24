afd2236ae7ef44b89e222714ab76e8c1ccc49998
Feilipu <Turgon.Telrunya@protonmail.com>
2021-02-24 16:05:16 +0100

    Création du fichier view/forum.php

--- view/forum.php
+++ view/forum.php
@@ -1,8 +1,11 @@
-<?php
-foreach ($messagesDesLeviathans as $messages) {
-	?>
-	<p> le <?= $messages->getDate(); ?> a été reçu ce message : </p>
-	<p><?= $messages->getTitle(); ?></p>
-	<p><?= $messages->getMessage(); ?></p>
-	<?php
-}
+<ul>
+	<li><a href=""></a>Bibliothèque à vapeur</li>
+	<li><a href=""></a>Mysterium</li>
+	<li><a href=""></a>Cinématographe</li>
+	<li><a href=""></a>Phonographe</li>
+	<li><a href=""></a>Salon de jeux</li>
+	<li><a href=""></a>Boutiques</li>
+	<li><a href=""></a>Evénements et sorties</li>
+	<li><a href=""></a>Clubs</li>
+	<li><a href=""></a>Bric à brac</li>
+</ul>
