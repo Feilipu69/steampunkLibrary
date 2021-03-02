f4e0626e413fe8331b5e8dda2291193dffcb6f91
Feilipu <Turgon.Telrunya@protonmail.com>
2021-03-02 10:30:38 +0100

    Mise à zéro de la classe OpinionManager

--- src/DAO/OpinionManager.php
+++ src/DAO/OpinionManager.php
@@ -6,13 +6,4 @@ use Bihin\steampunkLibrary\src\model\Opinion;
 
 class OpinionManager extends DbConnect
 {
-	public function getOpinions($isbn){
-		$opinions = [];
-		$req = $this->db->prepare('SELECT id, isbn, comment, DATE_FORMAT(dateOfComment, "%d/%m/%Y") AS dateOfComment FROM opinion WHERE isbn = ? ORDER BY id DESC');
-		$req->execute([$isbn]);
-		while($data = $req->fetch()){
-			$opinions[] = new Opinion($data);
-		}
-		return $opinions;
-	}
 }
