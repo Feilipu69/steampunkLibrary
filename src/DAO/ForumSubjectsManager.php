5accf34104f47cca690f75dfe5d08bc7a6237b1b
Feilipu <Turgon.Telrunya@protonmail.com>
2021-03-03 09:54:44 +0100

    Ajout de la classe Opinion ligne 11, ajout des méthodes getSubjectById, addOpinion et getOpinions

--- src/DAO/ForumSubjectsManager.php
+++ src/DAO/ForumSubjectsManager.php
@@ -6,7 +6,10 @@ use Bihin\steampunkLibrary\src\DAO\{
 	SubscriberManager
 };
 
-use Bihin\steampunkLibrary\src\model\ForumSubjects;
+use Bihin\steampunkLibrary\src\model\{
+	ForumSubjects,
+	Opinion
+};
 
 class ForumSubjectsManager extends DbConnect{
 	public function getSubject($parameter){
@@ -20,6 +23,16 @@ class ForumSubjectsManager extends DbConnect{
 		if (isset($subject)) {
 			return $subject;
 		}
+	}
+
+	public function getSubjectById($parameter){
+		$req = $this->db->prepare('SELECT id, loginSubscriber, subject, title, content, DATE_FORMAT(date, "%d/%m/%Y") AS date FROM forumSubjects WHERE id = ?');
+		$req->execute([
+			$parameter
+		]);
+		$data = $req->fetch();
+		$id = new ForumSubjects($data);
+		return $id;
 	}
 
 	public function addForumTheme($post){
@@ -42,6 +55,28 @@ class ForumSubjectsManager extends DbConnect{
 		}
 		if (isset($mySubjects)) {
 			return $mySubjects;
+		}
+	}
+
+	public function addOpinion($post, $parameter){
+		$req = $this->db->prepare('INSERT INTO opinions (login, idForum, comment, dateOfComment) VALUES (:login, :idForum, :comment, NOW())');
+		$req->execute([
+			':login' => $post['login'],
+			':idForum' => $parameter,
+			':comment' => $post['comment']
+		]);
+	}
+
+	public function getOpinions($parameter){
+		$req = $this->db->prepare('SELECT * FROM opinions WHERE idForum = ?');
+		$req->execute([
+			$parameter
+		]);
+		while ($data = $req->fetch()) {
+			$opinions[] = new Opinion($data);
+		}
+		if (isset($opinions)) {
+			return $opinions;
 		}
 	}
 }
