64da5248b0c4b4f14e009621b721ef7275be16c4
Feilipu <Turgon.Telrunya@protonmail.com>
2021-03-07 17:22:05 +0100

    Suppression de la condition if(isset())

--- view/book.php
+++ view/book.php
@@ -1,12 +1,3 @@
-<?php
-if (isset($opinion)) {
-	foreach($opinion as $opinions){
-		?>
-		<p><?= strip_tags($opinions->getComment()); ?></p>
-		<?php
-	}
-}
-?>
 <script type="text/javascript" src="https://www.google.com/books/jsapi.js"></script>
 <?php
 if (isset($_GET['parameter'])) {
