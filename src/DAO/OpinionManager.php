6abc0c8dc1e7d9363ee0ca3b172246490ff9486d
Feilipu <Turgon.Telrunya@protonmail.com>
2021-03-07 17:10:52 +0100

    ajout des méthodes addOpinion, getOpinions, opinionAgree, opinionDeleteAgree, opinionDisagree, opinionDeleteDisagree

--- src/DAO/OpinionManager.php
+++ src/DAO/OpinionManager.php
@@ -6,4 +6,65 @@ use Bihin\steampunkLibrary\src\model\Opinion;
 
 class OpinionManager extends DbConnect
 {
+	public function addOpinion($post, $parameter){
+		$req = $this->db->prepare('INSERT INTO opinions (login, idForum, comment, dateOfComment) VALUES (:login, :idForum, :comment, NOW())');
+		$req->execute([
+			':login' => $post['login'],
+			':idForum' => $parameter,
+			':comment' => $post['comment']
+		]);
+	}
+
+	public function getOpinions($parameter){
+		$req = $this->db->prepare('SELECT * FROM opinions WHERE idForum = ?');
+		$req->execute([
+			$parameter
+		]);
+		while ($data = $req->fetch()) {
+			$opinions[] = new Opinion($data);
+		}
+		if (isset($opinions)) {
+			return $opinions;
+		}
+	}
+
+	public function getAOpinion($parameter){
+		$req = $this->db->prepare('SELECT * FROM opinions WHERE id = ?');
+		$req->execute([
+			$parameter
+		]);
+		$data = $req->fetch();
+		$opinionData = new Opinion($data);
+		return $opinionData;
+	}
+
+	public function opinionAgree($parameter){
+		$req = $this->db->prepare('UPDATE opinions SET agree = ? WHERE id = ?');
+		$req->execute([
+			$_SESSION['subscriberId'],
+			$parameter
+		]);
+	}
+
+	public function opinionDeleteAgree($parameter){
+		$req = $this->db->prepare('UPDATE opinions SET agree = 0 WHERE id = ?');
+		$req->execute([
+			$parameter
+		]);
+	}
+
+	public function opinionDisagree($parameter){
+		$req = $this->db->prepare('UPDATE opinions SET disagree = ? WHERE id = ?');
+		$req->execute([
+			$_SESSION['subscriberId'],
+			$parameter
+		]);
+	}
+
+	public function opinionDeleteDisagree($parameter){
+		$req = $this->db->prepare('UPDATE opinions SET disagree = 0 WHERE id = ?');
+		$req->execute([
+			$parameter
+		]);
+	}
 }
