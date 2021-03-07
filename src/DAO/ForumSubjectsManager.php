6e59000dfcbac406a9f891f122052ceb0cf8a28b
Feilipu <Turgon.Telrunya@protonmail.com>
2021-03-07 17:07:45 +0100

    Déplacement des méthodes addOpinion et getOpinions dans OpinionManager

--- src/DAO/ForumSubjectsManager.php
+++ src/DAO/ForumSubjectsManager.php
@@ -74,26 +74,4 @@ class ForumSubjectsManager extends DbConnect{
 			$parameter
 		]);
 	}
-
-	public function addOpinion($post, $parameter){
-		$req = $this->db->prepare('INSERT INTO opinions (login, idForum, comment, dateOfComment) VALUES (:login, :idForum, :comment, NOW())');
-		$req->execute([
-			':login' => $post['login'],
-			':idForum' => $parameter,
-			':comment' => $post['comment']
-		]);
-	}
-
-	public function getOpinions($parameter){
-		$req = $this->db->prepare('SELECT * FROM opinions WHERE idForum = ?');
-		$req->execute([
-			$parameter
-		]);
-		while ($data = $req->fetch()) {
-			$opinions[] = new Opinion($data);
-		}
-		if (isset($opinions)) {
-			return $opinions;
-		}
-	}
 }
