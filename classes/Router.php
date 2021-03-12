b3212e96537d9f47cbc2188f8e20c28ff6e6e83a
Feilipu <Turgon.Telrunya@protonmail.com>
2021-03-12 14:32:07 +0100

    Modification de la présentation. Ajout de la route deleteOpinion

--- classes/Router.php
+++ classes/Router.php
@@ -19,88 +19,105 @@ class Router
 
 	public function renderController(){
 		if (isset($_GET['route'])) {
-			// render books
-			if ($_GET['route'] === 'books') {
-				$this->frontController->getBooks();
-			}
-			elseif ($_GET['route'] === 'book') {
-				$this->frontController->getABook($_GET['parameter']);
-			}
-			elseif ($_GET['route'] === 'addABook') {
-				$this->adminController->addABook($_POST);
-			}
-
-			// render member
-			if ($_GET['route'] === 'register') {
-				$this->frontController->register($_POST);
-			}
-			elseif ($_GET['route'] === 'connection') {
-				$this->frontController->connection($_POST);
-			}
-			elseif ($_GET['route'] === 'disconnection') {
-				$this->frontController->disconnection();
-			}
-			elseif ($_GET['route'] === 'updateData') {
-				$this->frontController->updateData($_POST);
-			}
-			elseif ($_GET['route'] === 'deleteSubscriber') {
-				$this->frontController->deleteSubscriber();
-			}
-
-			// render subjects and comments
-			if ($_GET['route'] === 'mySubjects') {
-				$this->frontController->mySubjects();
-			}
-			elseif ($_GET['route'] === 'subjectAndComments') {
-				$this->frontController->subjectAndComments($_POST, $_GET['parameter']);
-			}
-			elseif ($_GET['route'] === 'updateSubject') {
-				$this->frontController->updateSubject($_POST, $_GET['parameter']);
-			}
-			elseif ($_GET['route'] === 'deleteSubject') {
-				$this->frontController->deleteSubject($_GET['parameter']);
-			}
-
-			// agree disagree
-			if ($_GET['route'] === "addRemoveAgree") {
-				$this->frontController->addRemoveAgree($_GET['parameter']);
-			}
-			elseif ($_GET['route'] === "addRemoveDisagree") {
-				$this->frontController->addRemoveDisagree($_GET['parameter']);
-			}
-
-			// render administration
-			if ($_GET['route'] === 'administration') {
-				$this->adminController->administration();
-			}
-			elseif ($_GET['route'] === 'moderator') {
-				$this->adminController->moderator($_GET['parameter']);
-			}
-			elseif ($_GET['route'] === 'member') {
-				$this->adminController->member($_GET['parameter']);
-			}
-			elseif ($_GET['route'] === 'deleteMember') {
-				$this->adminController->deleteMember($_GET['parameter']);
-			}
-
-			// render newsletter
-			if ($_GET['route'] === 'newsletters') {
-				$this->frontController->newsletters();
-			}
-
-			// render forum
-			if ($_GET['route'] === 'forum') {
-				$this->frontController->forum();
-			}
-			elseif ($_GET['route'] === 'forumThemes') {
-				$this->frontController->forumThemes($_GET['parameter']);
-			}
-			elseif ($_GET['route'] === 'addForumTheme') {
-				$this->frontController->addForumTheme($_POST, $_GET['parameter']);
-			}
+			$this->renderBooks();
+			$this->renderMember();
+			$this->renderForum();
+			$this->renderSubjectAndComments();
+			$this->renderAgreeDisagree();
+			$this->renderAdministration();
+			$this->renderNewsLetter();
 		} else {
 			$display = new View('home');
 			$display->render([]);
+		}
+	}
+
+	public function renderBooks(){
+		if ($_GET['route'] === 'books') {
+			$this->frontController->getBooks();
+		}
+		elseif ($_GET['route'] === 'book') {
+			$this->frontController->getABook($_GET['parameter']);
+		}
+		elseif ($_GET['route'] === 'addABook') {
+			$this->adminController->addABook($_POST);
+		}
+	}
+
+	public function renderMember(){
+		if ($_GET['route'] === 'register') {
+			$this->frontController->register($_POST);
+		}
+		elseif ($_GET['route'] === 'connection') {
+			$this->frontController->connection($_POST);
+		}
+		elseif ($_GET['route'] === 'disconnection') {
+			$this->frontController->disconnection();
+		}
+		elseif ($_GET['route'] === 'updateData') {
+			$this->frontController->updateData($_POST);
+		}
+		elseif ($_GET['route'] === 'deleteSubscriber') {
+			$this->frontController->deleteSubscriber();
+		}
+	}
+
+	public function renderForum(){
+		if ($_GET['route'] === 'forum') {
+			$this->frontController->forum();
+		}
+		elseif ($_GET['route'] === 'forumThemes') {
+			$this->frontController->forumThemes($_GET['parameter']);
+		}
+		elseif ($_GET['route'] === 'addForumTheme') {
+			$this->frontController->addForumTheme($_POST, $_GET['parameter']);
+		}
+	}
+
+	public function renderSubjectAndComments(){
+		if ($_GET['route'] === 'mySubjects') {
+			$this->frontController->mySubjects();
+		}
+		elseif ($_GET['route'] === 'subjectAndComments') {
+			$this->frontController->subjectAndComments($_POST, $_GET['parameter'], $_GET['page']);
+		}
+		elseif ($_GET['route'] === 'updateSubject') {
+			$this->frontController->updateSubject($_POST, $_GET['parameter']);
+		}
+		elseif ($_GET['route'] === 'deleteSubject') {
+			$this->frontController->deleteSubject($_GET['parameter']);
+		}
+	}
+
+	public function renderAgreeDisagree(){
+		if ($_GET['route'] === "addRemoveAgree") {
+			$this->frontController->addRemoveOpinions($_GET['parameter'], 'agreeOpinions', 'subscriberIdAgree', 'agree', 'disagreeOpinions', 'subscriberIdDisagree', 'disagree');
+		} elseif ($_GET['route'] === 'addRemoveDisagree') {
+			$this->frontController->addRemoveOpinions($_GET['parameter'], 'disagreeOpinions', 'subscriberIdDisagree', 'disagree', 'agreeOpinions', 'subscriberIdAgree', 'agree');
+		}
+	}
+
+	public function renderAdministration(){
+		if ($_GET['route'] === 'administration') {
+			$this->adminController->administration();
+		}
+		elseif ($_GET['route'] === 'moderator') {
+			$this->adminController->moderator($_GET['parameter']);
+		}
+		elseif ($_GET['route'] === 'member') {
+			$this->adminController->member($_GET['parameter']);
+		}
+		elseif ($_GET['route'] === 'deleteMember') {
+			$this->adminController->deleteMember($_GET['parameter']);
+		}
+		elseif ($_GET['route'] === 'deleteOpinion') {
+			$this->adminController->deleteOpinion($_GET['parameter'], $_GET['page'], $_GET['opinionId']);
+		}
+	}
+
+	public function renderNewsletter(){
+		if ($_GET['route'] === 'newsletters') {
+			$this->frontController->newsletters();
 		}
 	}
 }
