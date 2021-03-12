1971fb2e1fd507e920c40e22455c34fdde58222e
Feilipu <Turgon.Telrunya@protonmail.com>
2021-03-12 14:33:08 +0100

    Modification des paramètres

--- src/DAO/AgreeDisagreeManager.php
+++ src/DAO/AgreeDisagreeManager.php
@@ -6,8 +6,17 @@ use Bihin\steampunkLibrary\src\model\AgreeDisagree;
 
 class AgreeDisagreeManager extends DbConnect 
 {
-	public function countAllAgreeOpinion($opinionId){
-		$req = $this->db->prepare('SELECT COUNT(*) FROM flagOpinions WHERE opinionId = ?');
+
+	public function getAllOpinions($opinionId, $table, $first, $quantity){
+		$req = $this->db->prepare('SELECT * FROM ' . $table . ' WHERE idForum = ? LIMIT ' . $first . ', ' . $quantity);
+		$req->execute([
+			$opinionId
+		]);
+		$data = $req->fetchAll();
+	}
+
+	public function countAllOpinions($opinionId, $table, $column){
+		$req = $this->db->prepare('SELECT COUNT(*) FROM ' . $table . ' WHERE ' . $column . ' = ?');
 		$req->execute([
 			$opinionId
 		]);
@@ -15,8 +24,8 @@ class AgreeDisagreeManager extends DbConnect
 		return $data;
 	}
 
-	public function countAgreeOpinion($opinionId){
-		$req = $this->db->prepare('SELECT COUNT(*) FROM flagOpinions WHERE subscriberIdAgree = ? AND opinionId = ?');
+	public function countOpinion($opinionId, $table, $column){
+		$req = $this->db->prepare('SELECT COUNT(*) FROM ' . $table . ' WHERE ' . $column . ' = ? AND opinionId = ?');
 		$req->execute([
 			$_SESSION['subscriberId'],
 			$opinionId
@@ -25,49 +34,15 @@ class AgreeDisagreeManager extends DbConnect
 		return $data;
 	}
 
-	public function addAgree($opinionId){
-		$req = $this->db->prepare('INSERT INTO flagOpinions (opinionId, subscriberLogin, forumSubjectsId, subscriberIdAgree) SELECT id, login, idForum, agree FROM opinions WHERE id = ?');
+	public function addOpinion($opinionId, $table, $column, $opinionsColumn){
+		$req = $this->db->prepare('INSERT INTO ' . $table . ' (opinionId, subscriberLogin, forumSubjectsId, ' . $column . ') SELECT id, login, idForum, ' . $opinionsColumn . ' FROM opinions WHERE id = ?');
 		$req->execute([
 			$opinionId
 		]);
 	}
 
-	public function removeAgree($opinionId){
-		$req = $this->db->prepare('DELETE FROM flagOpinions WHERE subscriberIdAgree = ? AND opinionId = ?');
-		$req->execute([
-			$_SESSION['subscriberId'],
-			$opinionId
-		]);
-	}
-
-	public function countAllDisagreeOpinion($opinionId){
-		$req = $this->db->prepare('SELECT COUNT(*) FROM flagOpinions WHERE opinionId = ?');
-		$req->execute([
-			$opinionId
-		]);
-		$data = $req->fetch();
-		return $data;
-	}
-
-	public function countDisagreeOpinion($opinionId){
-		$req = $this->db->prepare('SELECT COUNT(*) FROM flagOpinions WHERE subscriberIdDisagree = ? AND opinionId = ?');
-		$req->execute([
-			$_SESSION['subscriberId'],
-			$opinionId
-		]);
-		$data = $req->fetch();
-		return $data;
-	}
-
-	public function addDisagree($opinionId){
-		$req = $this->db->prepare('INSERT INTO flagOpinions (opinionId, subscriberLogin, forumSubjectsId, subscriberIdDisagree) SELECT id, login, idForum, disagree FROM opinions WHERE id = ?');
-		$req->execute([
-			$opinionId
-		]);
-	}
-
-	public function removeDisagree($opinionId){
-		$req = $this->db->prepare('DELETE FROM flagOpinions WHERE subscriberIdDisagree = ? AND opinionId = ?');
+	public function removeOpinion($opinionId, $table, $column){
+		$req = $this->db->prepare('DELETE FROM ' . $table . ' WHERE ' . $column . ' = ? AND opinionId = ?');
 		$req->execute([
 			$_SESSION['subscriberId'],
 			$opinionId
