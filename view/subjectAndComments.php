026d6386a82e5355efec32f1fd10cd523afdb505
Feilipu <Turgon.Telrunya@protonmail.com>
2021-03-20 18:22:00 +0100

    Essai ajax râté. Retrait de l'essai et retour au code premier

--- view/subjectAndComments.php
+++ view/subjectAndComments.php
@@ -35,22 +35,9 @@ if (isset($opinions)) {
 				<?php
 			}
 			?>
-
-			<button onclick="agree(<?= $opinion->getId(); ?>)"><img src="<?= HOST; ?>/public/thumbUp.svg" /></button> : <em><?= $opinion->getAgree()[0]; ?></em>
-			<button onclick="disagree(<?= $opinion->getId(); ?>)"><img src="<?= HOST; ?>/public/thumbDown.svg" /></button> : <em><?= $opinion->getDisagree()[0]; ?></em>
+			<em><a href="<?= HOST; ?>/addRemoveAgree/<?= $opinion->getId(); ?>/<?= $_GET['page']; ?>"><img src="<?= HOST; ?>/public/thumbUp.svg" /></a> : <?= $opinion->getAgree()[0]; ?></em>
+			<em><a href="<?= HOST; ?>/addRemoveDisagree/<?= $opinion->getId(); ?>/<?= $_GET['page']; ?>"><img src="<?= HOST; ?>/public/thumbDown.svg" /></a> : <?= $opinion->getDisagree()[0]; ?></em>
 		</div>
-		<script>
-			function agree(opinionId){
-				fetch("<?= HOST; ?>/addRemoveAgree/" + opinionId + "/<?= $_GET['page']; ?>")
-				//.then(response => console.log(response))
-			}
-
-			function disagree(opinionId){
-				fetch("<?= HOST; ?>/addRemoveDisagree/" + opinionId + "/<?= $_GET['page']; ?>")
-				//.then(response => console.log(response))
-			}
-		</script>
-
 		<?php
 	}
 	?>
