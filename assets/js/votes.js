67fcb0347d93953dc09bdabbc59ba0e727c02828
Feilipu <Turgon.Telrunya@protonmail.com>
2021-04-14 16:52:24 +0200

    Ajout du fichier votes.js

---
+++ assets/js/votes.js
@@ -0,0 +1,38 @@
+// Affichage de la page
+function getAllVotes(opinionId){
+	fetch(`${host}/getAllVotes/${opinionId}`)
+		.then(response => response.json())
+		.then(data => {
+			let agreeOpinions = 0;
+			let disagreeOpinions = 0;
+			for(vote of data){
+				if (vote.agree != 0) {
+					agreeOpinions = agreeOpinions + 1;
+				} 
+				if (vote.disagree != 0) {
+					disagreeOpinions = disagreeOpinions + 1;
+				} 
+			}
+			document.getElementById(`agreeOpinions${opinionId}`).textContent = agreeOpinions;
+			document.getElementById(`disagreeOpinions${opinionId}`).textContent = disagreeOpinions;
+		})
+}
+
+for(id of opinionsId){
+	getAllVotes(id);
+}
+
+// Modification des votes par l'utilisateur
+function addRemoveAgree(opinionId){
+	fetch(`${host}/addRemoveAgree/${opinionId}/agree`)
+		.then(response => {
+			getAllVotes(`${opinionId}`);
+		})
+}
+
+function addRemoveDisagree(opinionId){
+	fetch(`${host}/addRemoveDisagree/${opinionId}/disagree`)
+		.then(response => {
+			getAllVotes(`${opinionId}`);
+		})
+}
