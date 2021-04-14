11154548a89136e5d77eacf1de8af6e2bde27dcb
Feilipu <Turgon.Telrunya@protonmail.com>
2021-04-14 16:51:35 +0200

    Ajout de div container

--- view/template.php
+++ view/template.php
@@ -8,56 +8,62 @@
 		<title>La Bibliothèque à Vapeur</title>
 	</head>
 	<body>
-		<header>
-			<h1><a href="<?= HOST; ?>">La Bibliothèque à Vapeur</a></h1>
-			<nav>
-				<ul>
-					<li><a href="<?= HOST; ?>">Accueil</a></li>
-					<li><a href="<?= HOST; ?>/books">Bibliothèque</a></li>
-					<?php
-					if (isset($_SESSION['login'])) {
-						?>
-						<li><a href="<?= HOST; ?>/newsletters">Newsletter</a></li>
-						<li><a href="<?= HOST; ?>/forum">Forum</a></li>
+		<div class="container">
+			<header>
+				<h1><a href="<?= HOST; ?>">La Bibliothèque à Vapeur</a></h1>
+				<nav>
+					<ul>
+						<li><a href="<?= HOST; ?>">Accueil</a></li>
+						<li><a href="<?= HOST; ?>/books">Bibliothèque</a></li>
 						<?php
-						if (isset($_SESSION['role'])) {
+						if (isset($_SESSION['login'])) {
 							?>
-							<li><a href="<?= HOST; ?>/updateData">Gestion de vos données</a></li>
-							<li><a href="<?= HOST; ?>/mySubjects">Mes sujets</a></li>
+							<li><a href="<?= HOST; ?>/newsletters">Newsletter</a></li>
+							<li><a href="<?= HOST; ?>/forum">Forum</a></li>
+							<?php
+							if (isset($_SESSION['role'])) {
+								?>
+								<li><a href="<?= HOST; ?>/updateData">Gestion de vos données</a></li>
+								<li><a href="<?= HOST; ?>/mySubjects">Mes sujets</a></li>
+								<?php
+							}
+							if (isset($_SESSION['role']) && ($_SESSION['role'] === 'admin' || $_SESSION['role'] === 'moderateur')) {
+								?>
+								<li><a href="<?= HOST; ?>/administration">Administration</a></li>
+								<?php
+							}
+							?>
+							<li><a href="<?= HOST; ?>/disconnection">Déconnexion</a></li> 
 							<?php
 						}
-						if (isset($_SESSION['role']) && ($_SESSION['role'] === 'admin' || $_SESSION['role'] === 'moderateur')) {
-							?>
-							<li><a href="<?= HOST; ?>/administration">Administration</a></li>
-							<?php
-						}
-						?>
-						<li><a href="<?= HOST; ?>/disconnection">Déconnexion</a></li> 
-						<?php
-					}
 
-					if (!isset($_SESSION['login'])) {
+						if (!isset($_SESSION['login'])) {
+							?>
+							<li><a href="<?= HOST; ?>/register">Inscription</a></li>
+							<li><a href="<?= HOST; ?>/connection">Connexion</a></li>
+							<?php
+						}
 						?>
-						<li><a href="<?= HOST; ?>/register">Inscription</a></li>
-						<li><a href="<?= HOST; ?>/connection">Connexion</a></li>
-						<?php
-					}
-					?>
-				</ul>
-			</nav>
-		</header>
-		<section>
-			<?php
-			if (isset($_SESSION['login'])) {
-				?>
-				<p>Bienvenue <?= $_SESSION['login']; ?>
+					</ul>
+				</nav>
+			</header>
+		</div>
+		<div class="container">
+			<section>
 				<?php
-			}
-			?>
-			<?= $content; ?>
-		</section>
-		<footer>
-		</footer>
+				if (isset($_SESSION['login'])) {
+					?>
+					<p>Bienvenue <?= $_SESSION['login']; ?>
+					<?php
+				}
+				?>
+				<?= $content; ?>
+			</section>
+		</div>
+		<div class="container">
+			<footer>
+			</footer>
+		</div>
 		<script>
 			tinymce.init({
 				selector:'textarea',
