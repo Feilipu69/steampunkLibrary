838caed4797ae2a600e625763fff6a3e02ca6665
Feilipu <Turgon.Telrunya@protonmail.com>
2021-04-30 10:56:43 +0200

    Modification du nom du fichier getOneBook

--- assets/js/getOneBook.js
+++ assets/js/getOneBook.js
@@ -1,11 +1,13 @@
 let div = document.createElement("div");
 div.setAttribute("id", isbn);
+div.setAttribute("class", "container");
 let titleElement = document.createElement("p");
 let img = document.createElement("img");
 let authorElement = document.createElement("p");
 let publisherElement = document.createElement("p");
 let publishedDateElement = document.createElement("p");
 let descriptionElement = document.createElement("p");
+descriptionElement.setAttribute("id", "description");
 fetch("https://www.googleapis.com/books/v1/volumes?q=" + isbn)
 	.then((response) => response.json())
 	.then((data) => {
@@ -16,6 +18,8 @@ fetch("https://www.googleapis.com/books/v1/volumes?q=" + isbn)
 		img.src = data.items[0].volumeInfo.imageLinks.thumbnail;
 		img.alt = "couverture " + data.items[0].volumeInfo.title;
 		descriptionElement.textContent = data.items[0].volumeInfo.description;
+		descriptionElement.style.fontFamily = "ubunut";
+		descriptionElement.style.fontSize = "20px";
 		document.body.appendChild(div);
 		div.appendChild(titleElement);
 		div.appendChild(authorElement);
