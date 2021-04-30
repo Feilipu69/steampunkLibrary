4d019039a4a3cc9bfc640052753b77ab4a2e29a2
Feilipu <Turgon.Telrunya@protonmail.com>
2021-04-30 11:06:16 +0200

    Modification du lien 'A' devient One

--- view/book.php
+++ view/book.php
@@ -3,7 +3,7 @@
 if (isset($_GET['parameter'])) {
 	?>
 	<script>let isbn = <?= $_GET['parameter'] ?></script>
-	<script src="../assets/js/getABook.js"></script>
+	<script src="../assets/js/getOneBook.js"></script>
 	<?php
 }
 ?>
