4fb36c6c58841d9a30c1fc2271be622125cfd54b
Feilipu <Turgon.Telrunya@protonmail.com>
2021-05-02 16:25:15 +0200

    Lignes 5 à 10 : Ajout de div pour l'affichage des livres

--- view/books.php
+++ view/books.php
@@ -1,8 +1,18 @@
+<div class="row">
 <?php
 foreach ($catalogue as $book) {
+	?>
+	<div id="cardDeck" class="card-deck col-md-4">
+		<div id="card<?= $book->getIsbn(); ?>" class="card mb-3">
+			<div id="cardHeader<?= $book->getIsbn(); ?>" class="card-header"></div>
+			<div id="cardBody<?= $book->getIsbn(); ?>" class="card-body"></div>
+		</div>
+	</div>
+	<?php
 	$list[] = $book->getIsbn();
 }
 ?>
+</div>
 <script type="text/javascript" src="https://www.google.com/books/jsapi.js"></script>
 <script>
 	let host = "<?= HOST; ?>";
