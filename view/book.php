a19cfb912843db31ded20dc6727a6521d96341e9
Feilipu <Turgon.Telrunya@protonmail.com>
2021-05-04 16:00:19 +0200

    Nouveau code html pour afficher un livre

--- view/book.php
+++ view/book.php
@@ -1,7 +1,21 @@
-<p><a href="<?= HOST; ?>/books">Retour</a></p>
+<p class="container"><a href="<?= HOST; ?>/books">Retour</a></p>
 <?php
 if (isset($_GET['parameter'])) {
 	?>
+	<div id="isbn" class="container">
+		<div class="d-flex">
+			<div class="mr-5">
+				<p id="title"></p>
+				<p id="publisher"></p>
+			</div>
+			<div>
+				<p id="author"></p>
+				<p id="date"></p>
+			</div>
+		</div>
+		<div id="image"></div>
+		<p id="bookDescription"></p>
+	</div>
 	<script>let isbn = <?= $_GET['parameter'] ?></script>
 	<script src="../assets/js/getOneBook.js"></script>
 	<?php
