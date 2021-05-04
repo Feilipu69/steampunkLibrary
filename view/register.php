7f917ea247f518e9ecf32f36c4b88cc4446a65e3
Feilipu <Turgon.Telrunya@protonmail.com>
2021-05-04 16:03:54 +0200

    Mise en page avec ajout de balises, classes et d'une image

--- view/register.php
+++ view/register.php
@@ -1,17 +1,27 @@
-<h3>Inscription</h3>
-<form method="post" action="<?= HOST; ?>/register">
-	<label for="login">Pseudo : </label>
-	<input type="text" name="login" required />
-	<br>
-	<label for="password">Mot de passe : </label>
-	<input type="password" name="password" required />
-	<br>
-	<label for="mail">Courriel : </label>
-	<input type="email" name="email" required />
-	<br>
-	<input type="submit" name="register" value="Inscription" />
-	<button onclick="window.location.href='<?= HOST; ?>/register';">Annuler</button>
-</form>
+<div class="d-flex flex-column flex-md-row">
+	<div>
+		<img src="<?= HOST; ?>/public/smallBalloon.png" alt="Image d'un dirigeable steampunk" class="img-fluid"/>
+	</div>
+	<div class="pt-5">
+		<h3>Inscription</h3>
+		<form method="post" action="<?= HOST; ?>/register">
+			<div class="form-group">
+			<label for="login">Pseudo : </label>
+			<input type="text" name="login" required class="form-control" />
+			</div>
+			<div class="form-group">
+			<label for="password">Mot de passe : </label>
+			<input type="password" name="password" required class="form-control" />
+			</div>
+			<div class="form-group">
+			<label for="mail">Courriel : </label>
+			<input type="email" name="email" required class="form-control" />
+			</div>
+			<input type="submit" id="submit" name="register" value="Inscription" />
+			<button id="cancel" onclick="window.location.href='<?= HOST; ?>/register';">Annuler</button>
+		</form>
+	</div>
+</div>
 
 <?php
 if (isset($_SESSION['registerError'])) {
