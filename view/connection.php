ae1d7471c038127f84247163bcab17282cf65a8f
Feilipu <Turgon.Telrunya@protonmail.com>
2021-05-04 16:02:09 +0200

    Mise en page avec ajout de balises, de class et d'une image

--- view/connection.php
+++ view/connection.php
@@ -1,14 +1,23 @@
-<h3>Connexion</h3>
-<form method="post" action="<?= HOST; ?>/connection">
-	<label for="login">Login :</label>
-	<input type="text" name="login" required />
-	<br>
-	<label for="password">Mot de passe : </label>
-	<input type="password" name="password" required />
-	<br>
-	<input type="submit" name="connection" value="Connexion" />
-	<button onclick="window.location.href='<?= HOST; ?>/connection';">Annuler</button>
-</form>
+<div class="container d-flex flex-column flex-md-row">
+	<div class="pt-5">
+		<h3>Connexion</h3>
+		<form method="post" action="<?= HOST; ?>/connection">
+			<div class="form-group">
+			<label for="login">Login :</label>
+			<input type="text" name="login" required class="form-control" />
+			</div>
+			<div class="form-group">
+			<label for="password">Mot de passe : </label>
+			<input type="password" name="password" required class="form-control" />
+			</div>
+			<input type="submit" id="submit" name="connection" value="Connexion" />
+			<button id="cancel" onclick="window.location.href='<?= HOST; ?>/connection';">Annuler</button>
+		</form>
+	</div>
+	<div>
+		<img src="<?= HOST; ?>/public/smallBoat.png" alt="image d'un bateau steampunk" class="img-fluid" />
+	</div>
+</div>
 
 <?php
 if (isset($_SESSION['error'])) {
