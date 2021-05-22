ad29fd19c1136a106326c5a1d03007f3f94c8f81
Feilipu <Turgon.Telrunya@protonmail.com>
2021-05-22 15:44:57 +0200

    Parameter devient isbn

--- assets/js/getBooks.js
+++ assets/js/getBooks.js
@@ -1,10 +1,10 @@
 async function livres() {
-	for(parameter of catalogue){
-		await fetch("https://www.googleapis.com/books/v1/volumes?q=" + parameter)
+	for(isbn of catalogue){
+		await fetch("https://www.googleapis.com/books/v1/volumes?q=" + isbn)
 			.then((response) => response.json())
 			.then(data => {
-				document.getElementById("cardHeader" + parameter).textContent = data.items[0].volumeInfo.title;
-				document.getElementById("cardBody" + parameter).innerHTML = "<a href=" + `${host}/book/${parameter}` + "><img src=" + data.items[0].volumeInfo.imageLinks.thumbnail + " alt = 'couverture '" + data.items[0].volumeInfo.title + " /></a>";
+				document.getElementById("cardHeader" + isbn).textContent = data.items[0].volumeInfo.title;
+				document.getElementById("cardBody" + isbn).innerHTML = "<a href=" + `${host}/book/${isbn}` + "><img src=" + data.items[0].volumeInfo.imageLinks.thumbnail + " alt = 'couverture '" + data.items[0].volumeInfo.title + " /></a>";
 			})
 	}
 }
