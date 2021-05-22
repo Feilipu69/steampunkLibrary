146a8866f74bfc294e58c256bf05d23cc142b48a
Feilipu <Turgon.Telrunya@protonmail.com>
2021-05-22 15:56:30 +0200

    Modification de la méthode getBooks et création des méthodes catalogue et countAllBooks pour la pagination de la page Bibiliothèque

--- src/DAO/BooksCatalogueManager.php
+++ src/DAO/BooksCatalogueManager.php
@@ -6,13 +6,27 @@ use Bihin\steampunkLibrary\src\model\BooksCatalogue;
 
 class BooksCatalogueManager extends DbConnect
 {
-	public function getBooks(){
-		$req = $this->db->query('SELECT * FROM booksCatalogue ORDER BY id DESC');
+	public function catalogue(){
+		$req = $this->db->query('SELECT * FROM booksCatalogue');
+		$data = $req->fetch();
+		$catalogue[] = new BooksCatalogue($data);
+		return $catalogue;
+	}
+
+	public function getBooks($first, $byPage){
+		$req = $this->db->query('SELECT * FROM booksCatalogue ORDER BY id DESC LIMIT ' . $first . ', ' . $byPage);
 		while ($data = $req->fetch()) {
 			$books[] = new BooksCatalogue($data);
 		}
 		return $books;
 	}
+
+	public function countAllBooks(){
+		$req = $this->db->query('SELECT COUNT(*) FROM booksCatalogue');
+		$allBooks = $req->fetch();
+		return $allBooks[0];
+	}
+
 	public function addOneBook($post){
 		$book = new BooksCatalogue($post);
 		$req = $this->db->prepare('INSERT INTO booksCatalogue (isbn) VALUES (:isbn)');
