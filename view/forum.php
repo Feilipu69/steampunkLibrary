db4dbb2f66204c2ae04fa21ca5589d5260d7c7d6
Feilipu <Turgon.Telrunya@protonmail.com>
2021-05-24 09:51:11 +0200

    Ajout d'un background-image marbre pour le fond des cartes (livres) et du menu du forum

--- view/forum.php
+++ view/forum.php
@@ -1,6 +1,6 @@
 <h3 class="container">Forum</h3>
-<div class="section">
-	<img src="<?= HOST; ?>/public/steampunk-border.png" alt="Bordure" class=" border1 img-fluid" />
+<div class="section" id="forum">
+	<img src="<?= HOST; ?>/public/steampunk-border.png" alt="Bordure" class=" border1 img-fluid mt-5" />
 	<nav class="mt-5 mb-5">
 		<ul class="nav justify-content-center">
 			<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/forumThemes/bibliotheque">Bibliothèque</a></li>
@@ -13,5 +13,5 @@
 			<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/forumThemes/bazar">Bazar</a></li>
 		</ul>
 	</nav>
-	<img src="<?= HOST; ?>/public/steampunk-border.png" alt="Bordure" class="border2 img-fluid" />
+	<img src="<?= HOST; ?>/public/steampunk-border.png" alt="Bordure" class="border2 img-fluid mb-5" />
 </div>
