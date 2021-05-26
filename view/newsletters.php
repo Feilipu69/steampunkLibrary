ab1721f94e8df1e29b325dcb0aabf2846e652e02
Feilipu <Turgon.Telrunya@protonmail.com>
2021-05-26 17:46:25 +0200

    Centrage de la fenêtre de la newletter

--- view/newsletters.php
+++ view/newsletters.php
@@ -1,3 +1,5 @@
+<div class="newsletter">
 <iframe class="mj-w-res-iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://app.mailjet.com/widget/iframe/6wGk/HS7" width="100%"></iframe>
+</div>
 
 <script type="text/javascript" src="https://app.mailjet.com/statics/js/iframeResizer.min.js"></script>
