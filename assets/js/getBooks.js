5f934353868bc602681902a8bcf34238925d55c6
Feilipu <Turgon.Telrunya@protonmail.com>
2021-05-28 11:10:20 +0200

    Ajout de la fermeture des guillemets à la fin des url

--- assets/js/getBooks.js
+++ assets/js/getBooks.js
@@ -1,6 +1,6 @@
 async function livres() {
 	for(isbn of catalogue){
-		await fetch("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn)
+		await fetch("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn + "")
 			.then((response) => response.json())
 			.then(data => {
 				document.getElementById("cardHeader" + isbn).textContent = data.items[0].volumeInfo.title;
