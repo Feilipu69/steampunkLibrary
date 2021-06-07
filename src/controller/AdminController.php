9cfd76081a07d7d27116c5047f315d8caebca545
Feilipu <Turgon.Telrunya@protonmail.com>
2021-06-07 15:09:37 +0200

    Ajout de la méthode checkRole pour une plus grande sécurité et indentation

--- src/controller/AdminController.php
+++ src/controller/AdminController.php
@@ -12,33 +12,43 @@ use Bihin\steampunkLibrary\utils\View;
 
 class AdminController
 {	
-	/**
-	 * administration. Affiche un formulaire d'entrée d'un nouveau livre, la gestion du role des membres et la possibilité de supprimer un membre.
-	 *
-	 * @return void
-	 */
-	public function administration(){
-		$subscribers = new SubscriberManager();
-		$allSubscribers = $subscribers->getSubscribers();
-
-		$subscriberId = [];
-		foreach ($allSubscribers as $subscriber) {
-			$subscribersId[] = $subscriber->getId();
+	public function checkRole(){
+		if ($_SESSION['role'] !== 'admin' && $_SESSION['role'] !== 'moderator') {
+			header('Location:' . HOST);
+		} else {
+			return true;
 		}
-		
-		$displayData = new View('administration');
-		$displayData->render([
-			'allSubscribers' => $allSubscribers,
-			'subscribersId' => json_encode($subscribersId)
-		]);
 	}
-	
+
 	/**
-	 * addABook ajoute un livre à la base de données
-	 *
-	 * @param  mixed $book
-	 * @return void
-	 */
+	* administration. Affiche un formulaire d'entrée d'un nouveau livre, la gestion du role des membres et la possibilité de supprimer un membre.
+	*
+	* @return void
+	*/
+	public function administration(){
+		if ($this->checkRole()) {
+			$subscribers = new SubscriberManager();
+			$allSubscribers = $subscribers->getSubscribers();
+
+			$subscriberId = [];
+			foreach ($allSubscribers as $subscriber) {
+				$subscribersId[] = $subscriber->getId();
+			}
+
+			$displayData = new View('administration');
+			$displayData->render([
+				'allSubscribers' => $allSubscribers,
+				'subscribersId' => json_encode($subscribersId)
+			]);
+		}
+	}
+
+	/**
+	* addABook ajoute un livre à la base de données
+	*
+	* @param  mixed $book
+	* @return void
+	*/
 	public function addOneBook($book){
 		if (isset($book['addOneBook'])) {
 			if (!empty($book['isbn'])) {
@@ -48,61 +58,61 @@ class AdminController
 		}
 		$this->administration();
 	}
-	
+
 	/**
-	 * getRole récupère les rôles des membres
-	 *
-	 * @param  int $id
-	 * @return void
-	 */
+	* getRole récupère les rôles des membres
+	*
+	* @param  int $id
+	* @return void
+	*/
 	public function getRole($id){
 		$subscriber = new SubscriberManager();
 		$role = $subscriber->getRole($id);
 		echo json_encode($role);
 	}
-	
+
 	/**
-	 * moderator permet à un membre de devenir modérateur
-	 *
-	 * @param  int $id
-	 * @return void
-	 */
+	* moderator permet à un membre de devenir modérateur
+	*
+	* @param  int $id
+	* @return void
+	*/
 	public function moderator($id){
 		$moderator = new SubscriberManager();
 		$newModerator = $moderator->moderator($id);
 	}
-	
+
 	/**
-	 * member retour d'un utilisateur au rôle de membre
-	 *
-	 * @param  int $id
-	 * @return void
-	 */
+	* member retour d'un utilisateur au rôle de membre
+	*
+	* @param  int $id
+	* @return void
+	*/
 	public function member($id){
 		$member = new SubscriberManager();
 		$newMember = $member->member($id);
 	}
-	
+
 	/**
-	 * deleteMember supprime un membre
-	 *
-	 * @param  int $id
-	 * @return void
-	 */
+	* deleteMember supprime un membre
+	*
+	* @param  int $id
+	* @return void
+	*/
 	public function deleteMember($id){
 		$member = new SubscriberManager();
 		$deleteMember = $member->deleteMember($id);
 		header('Location:' . HOST . '/administration');
 	}
-	
+
 	/**
-	 * deleteOpinion supprime un mauvais commentaire
-	 *
-	 * @param  mixed $parameter
-	 * @param  int $page
-	 * @param  int $id
-	 * @return void
-	 */
+	* deleteOpinion supprime un mauvais commentaire
+	*
+	* @param  mixed $parameter
+	* @param  int $page
+	* @param  int $id
+	* @return void
+	*/
 	public function deleteOpinion($parameter, $page, $id){
 		$opinion = new OpinionManager();
 		$deleteOpinion = $opinion->deleteOpinion($id);
