77dda55b50d2fc2a44495f28853db1faca617655
Feilipu <Turgon.Telrunya@protonmail.com>
2021-06-09 10:23:10 +0200

    Mise en page du formulaire

--- view/updatePostFormular.php
+++ view/updatePostFormular.php
@@ -1,16 +1,18 @@
 <?php
 if (isset($_GET['parameter'])) {
 	?>
-	<form method="post" action="<?= HOST; ?>/updatePost/<?= $_GET['parameter']; ?>">
-		<label for="title">Titre</label>
-		<input type="text" name="title" id="title" />
-		<br>
-		<label for="content">Contenu</label>
-		<textarea name="content"></textarea>
-		<br>
-		<input type="submit" name="send" value="Envoyer" />
-		<button onclick="window.location.href='<?= HOST; ?>/updatePost/<?= $_GET['parameter']; ?>';">Annuler</button>
-	</form>
+	<div class="container">
+		<form method="post" action="<?= HOST; ?>/updatePost/<?= $_GET['parameter']; ?>">
+			<label for="title">Titre</label>
+			<input type="text" name="title" id="title" />
+			<br>
+			<label for="content">Contenu</label>
+			<textarea name="content"></textarea>
+			<br>
+			<input type="submit" name="send" value="Envoyer" />
+			<button onclick="window.location.href='<?= HOST; ?>/updatePost/<?= $_GET['parameter']; ?>';">Annuler</button>
+		</form>
+	</div>
 	<?php
 }
 ?>
