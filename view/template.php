3ad82b71089b382462db565f715cfe59f779f70c
Feilipu <Turgon.Telrunya@protonmail.com>
2021-06-22 10:53:47 +0200

    Correction de l'indentation

--- view/template.php
+++ view/template.php
@@ -16,79 +16,78 @@
 			<header class="pb-3 mb-3">
 				<div class="text-center mt-5"><img src="<?= HOST; ?>/public/gear-animated.gif" alt="" class="d-none d-lg-inline img-fluid" /><h1 class="d-inline"><a href="<?= HOST; ?>" class="text-decoration-none">La Bibliothèque à Vapeur</a></h1><img src="<?= HOST; ?>/public/gear-animated.gif" alt="" id="gear" class="d-none d-lg-inline img-fluid" /></div>
 				<nav id="navbar" class="navbar navbar-expand-md navbar-light border-top border-bottom border-warning">
-					<button id="buttonBurger" class="navbar-toggler mx-auto" type="button" data-toggle="collapse" data-target="#navbarContent">
-						<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-							<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-							</svg>
-							Menu
-						</button>
-						<div id="navbarContent" class="collapse navbar-collapse justify-content-center">
-							<ul class="nav">
-								<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>">Accueil</a></li>
-								<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/books?page=1">Bibliothèque</a></li>
+					<button id="buttonBurger" class="navbar-toggler mx-auto" type="button" data-toggle="collapse" data-target="#navbarContent"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+						<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+						</svg>
+						Menu
+					</button>
+					<div id="navbarContent" class="collapse navbar-collapse justify-content-center">
+						<ul class="nav">
+							<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>">Accueil</a></li>
+							<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/books?page=1">Bibliothèque</a></li>
+							<?php
+							if (isset($_SESSION['login'])) {
+								?>
+								<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/newsletters">Bulletin</a></li>
+								<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/forum">Forum</a></li>
+								<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/updateData">Gestion de vos données</a></li>
+								<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/myPosts">Mes sujets</a></li>
 								<?php
-								if (isset($_SESSION['login'])) {
+								if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin' || $_SESSION['role'] === 'moderator') {
 									?>
-									<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/newsletters">Bulletin</a></li>
-									<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/forum">Forum</a></li>
-									<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/updateData">Gestion de vos données</a></li>
-									<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/myPosts">Mes sujets</a></li>
-									<?php
-									if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin' || $_SESSION['role'] === 'moderator') {
-										?>
-										<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/administration">Administration</a></li>
-										<?php
-									}
-									?>
-									<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/disconnection">Déconnexion</a></li> 
-									<?php
-								}
-								if (!isset($_SESSION['login'])) {
-									?>
-									<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/register">Inscription</a></li>
-									<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/connection">Connexion</a></li>
+									<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/administration">Administration</a></li>
 									<?php
 								}
 								?>
-							</ul>
-						</div>
-					</nav>
-				</header>
-			</div>
-			<main class="container rounded pt-3 pb-3">
-				<?php
-				if (isset($_SESSION['login'])) {
-					?>
-					<div class="bienvenue container mt-3 mb-3">Bienvenue <?= $_SESSION['login']; ?></div>
-					<?php
-				}
-				?>
-				<?= $content; ?>
-			</main>
-			<footer class="mt-3">
-				<div class="container rounded d-lg-flex justify-content-between">
-					<div>
-						Liens :
-						<ul>
-							<li><a href="https://fr.wikipedia.org/wiki/Steampunk" target="_blank">Steampunk Wikipedia</a></li> 
-							<li><a href="https://www.french-steampunk.fr" target="_blank">french-steampunk</a></li>
-							<li><a href="https://www.steampunkstore.fr" target="_blank">steampunkstore</a></li>
+								<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/disconnection">Déconnexion</a></li> 
+								<?php
+							}
+							if (!isset($_SESSION['login'])) {
+								?>
+								<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/register">Inscription</a></li>
+								<li class="nav-item"><a class="nav-link" href="<?= HOST; ?>/connection">Connexion</a></li>
+								<?php
+							}
+							?>
 						</ul>
 					</div>
-					<div>
-						<div class="author">&copy; 2021 Apprenti-développeur</div>
-					</div>
+				</nav>
+			</header>
+		</div>
+		<main class="container rounded pt-3 pb-3">
+			<?php
+			if (isset($_SESSION['login'])) {
+				?>
+				<div class="bienvenue container mt-3 mb-3">Bienvenue <?= $_SESSION['login']; ?></div>
+				<?php
+			}
+			?>
+			<?= $content; ?>
+		</main>
+		<footer class="mt-3">
+			<div class="container rounded d-lg-flex justify-content-between">
+				<div>
+					Liens :
+					<ul>
+						<li><a href="https://fr.wikipedia.org/wiki/Steampunk" target="_blank">Steampunk Wikipedia</a></li> 
+						<li><a href="https://www.french-steampunk.fr" target="_blank">french-steampunk</a></li>
+						<li><a href="https://www.steampunkstore.fr" target="_blank">steampunkstore</a></li>
+					</ul>
 				</div>
-			</footer>
-			<script>
-				tinymce.init({
-					selector:'textarea',
-					plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-					toolbar_mode: 'floating'
-				});
-			</script>
-			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-		</body>
-	</html>
+				<div>
+					<div class="author">&copy; 2021 Apprenti-développeur</div>
+				</div>
+			</div>
+		</footer>
+		<script>
+			tinymce.init({
+				selector:'textarea',
+				plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+				toolbar_mode: 'floating'
+			});
+		</script>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	</body>
+</html>
